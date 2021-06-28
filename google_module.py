7edfb6231b462a01ced69c49c6563f5624419edb
PARTHIPAN KANNAN <66922169+DONTSAYCANT@users.noreply.github.com>
2021-06-28 21:33:17 +0530

    June28

---
+++ google_module.py
@@ -0,0 +1,9 @@
+def function1(val1, val2):
+    return val1 + val2 * 3
+
+def function3(val1, val2):
+    return val1 * val2
+
+
+def function5(val1, val2, val3):
+    return val1[0] + val2[-1] + val3[0]
